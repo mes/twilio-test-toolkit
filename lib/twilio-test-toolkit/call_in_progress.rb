01476d7b1a0febe33402878de1e01f1475ddc8af
Patrick Gibson <patrick@patrickg.com>
2014-07-17 16:48:30 -0700

    Ensure options passed to tty_call make it through to request_for_twiml! Some were lost in CallInProgress (i.e. :call_status).

--- lib/twilio-test-toolkit/call_in_progress.rb
+++ lib/twilio-test-toolkit/call_in_progress.rb
@@ -3,6 +3,7 @@ require "twilio-test-toolkit/call_scope"
 module TwilioTestToolkit
   # Models a call
   class CallInProgress < CallScope
+    attr_reader :options
     attr_reader :sid, :initial_path, :http_method
     attr_reader :from_number, :to_number
     attr_reader :is_machine, :called, :direction
@@ -12,14 +13,22 @@ module TwilioTestToolkit
     # * :call_sid - specify an optional fixed value to be passed as params[:CallSid]
     # * :is_machine - controls params[:AnsweredBy]
     def initialize(initial_path, from_number, to_number, options = {})
+      default_options = {
+        :method     => :post,
+        :direction  => 'inbound',
+        :is_machine => false
+      }
+
+      @options = default_options.merge(options)
+
       # Save our variables for later
       @initial_path = initial_path
       @from_number  = from_number
       @to_number    = to_number
-      @is_machine   = options[:is_machine]
-      @called       = options[:called]
-      @direction    = options[:direction]
-      @http_method  = options[:method] || :post
+      @is_machine   = @options[:is_machine]
+      @called       = @options[:called]
+      @direction    = @options[:direction]
+      @http_method  = @options[:method]
 
       # Generate an initial call SID if we don't have one
       if (options[:call_sid].nil?)
@@ -32,12 +41,7 @@ module TwilioTestToolkit
       self.root_call = self
 
       # Create the request
-      request_for_twiml!(@initial_path,
-                         :method     => @http_method,
-                         :is_machine => @is_machine,
-                         :called     => @called,
-                         :direction  => @direction
-                        )
+      request_for_twiml!(@initial_path, @options)
     end
 
   end
